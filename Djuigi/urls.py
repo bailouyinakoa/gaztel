79d1c3150d18a864805379689e7f865159bf7dff
bailouyinakoa <yinakoab@gmail.com>
2025-07-02 15:41:05 +0000

    Update urls.py

--- Djuigi/urls.py
+++ Djuigi/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import *
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('elinels/', admin.site.urls),
     path('',include("DjuigiFaune.urls")),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+staticfiles_urlpatterns()
