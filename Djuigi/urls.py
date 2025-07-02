49794cb9920c783ef8a2ba02b12f4112f3754b4d
PROSPER <yinakoab@email.com>
2025-07-02 15:38:45 +0000

    fetch first

--- Djuigi/urls.py
+++ Djuigi/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 urlpatterns = [
     path('elinels/', admin.site.urls),
     path('',include("DjuigiFaune.urls")),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+staticfiles_urlpatterns()
