fd732a6f56ef49eaeba1fb6d169276a10c8a4775
PROSPER <yinakoab@email.com>
2025-07-02 15:05:07 +0000

    modification lien

--- Djuigi/urls.py
+++ Djuigi/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 from django.conf.urls.static import *
 from django.conf import settings
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('elinels/', admin.site.urls),
     path('',include("DjuigiFaune.urls")),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
